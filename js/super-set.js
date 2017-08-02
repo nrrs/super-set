e3746a1989faf8a5c94a35dbe302defdad18c2a4
Norris Kwan <norris.kwan@gmail.com>
2017-08-02 15:35:08 -0700

    Refactor binding for redeal

--- js/super-set.js
+++ js/super-set.js
@@ -3,43 +3,61 @@ let selectedCards = [];
 let currentBoard = [];
 let setScore = 0;
 
-class Game {
+function shuffle(cards) {
+  let currentIdx = cards.length;
+  let tempVal, randomIdx;
+  while (currentIdx !== 0) {
+    randomIdx = Math.floor(Math.random() * currentIdx);
+    currentIdx -= 1;
+    tempVal = cards[currentIdx];
+    cards[currentIdx] = cards[randomIdx];
+    cards[randomIdx] = tempVal;
+  }
+  return cards;
+}
 
+class Game {
   constructor() {
     const deck = new Deck();
     this.deck = deck;
     this.deal(12, deck);
+    document.getElementById('deal-button').onclick = this.redeal.bind(this);
+  }
+
+  redeal() {
+    this.deck = this.deck.concat(currentBoard);
+    shuffle(this.deck);
+    document.querySelectorAll('.card-container').forEach (el => {
+      el.removeChild(el.childNodes[0]);
+    });
+    this.deal(12);
   }
 
   evaluateSet(cards) {
-    if ( game.shapeSet(cards) &&
-        game.colorsSet(cards) &&
-        game.patternsSet(cards) &&
-        game.numbersSet(cards) ) {
+    if ( this.shapeSet(cards) &&
+        this.colorsSet(cards) &&
+        this.patternsSet(cards) &&
+        this.numbersSet(cards) ) {
       this.flash('success', 'Valid Set!');
-      game.addPoints();
+      this.addPoints();
       setTimeout(() => { // Update the board after 1.5s of animations
         cards.forEach ( (card, i) => {
           const bye = document.querySelector(`[data-id='${cards[i].id}']`);
           const currId = cards[i].id;
-
           currentBoard.forEach( boardElement => {
             if (boardElement.id === currId) {
               const remove = currentBoard.indexOf(boardElement);
               currentBoard.splice(remove, 1);
             }
           });
-
           if (setScore < 24) { // If set count < 24, keep filling the board.
-            const drawn = game.deck.shift();
+            const drawn = this.deck.shift();
             currentBoard.push(drawn);
-
-            const newCard = game.createNewCard(drawn, i);
+            const newCard = this.createNewCard(drawn, i);
             bye.parentNode.replaceChild(newCard, bye);
           } else {  // Deck is empty. Clean up the board.
             bye.parentNode.removeChild(bye);
           }
-
         });
       }, 1500);
     } else {
@@ -170,15 +188,15 @@ class Game {
     document.getElementById('points').value = setScore;
   }
 
-  deal(n, deck) {
+  deal(n) {
+  // deal(n, deck) {
+    currentBoard = [];
     const nArray = [];
-
     for (let i = 0; i < n; i++) {
-      const card = deck.pop();
+      const card = this.deck.shift();
       currentBoard.push(card);
       nArray.push(card);
     }
-
     nArray.forEach( (card, i) => {
       const cardElement = this.createNewCard(card, i);
       document.getElementById(`card-${i+1}`).appendChild(cardElement);
@@ -205,23 +223,7 @@ class Deck {
       ))
     ));
 
-    return this.shuffle(this.cards);
-  }
-
-  shuffle(cards) {
-    let currentIdx = cards.length;
-    let tempVal, randomIdx;
-
-    while (currentIdx !== 0) {
-      randomIdx = Math.floor(Math.random() * currentIdx);
-      currentIdx -= 1;
-
-      tempVal = cards[currentIdx];
-      cards[currentIdx] = cards[randomIdx];
-      cards[randomIdx] = tempVal;
-    }
-
-    return cards;
+    return shuffle(this.cards);
   }
 }
 
